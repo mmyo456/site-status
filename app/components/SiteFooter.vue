19ff276d4d39ccd1f440dd7baff766adca3fad23
鸭鸭 <86056701+mmyo456@users.noreply.github.com>
2025-01-29 06:51:35 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -32,7 +32,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        <n-text depth="3" @click="jumpLink(linkData.home)"> 鸭鸭 </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
@@ -51,8 +51,8 @@ const { siteIcp, version } = configPublic;
 
 const linkData = {
   github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  home: "https://www.ouo.chat/",
+  email: "qazwsx110as@163.com",
 };
 </script>
 
